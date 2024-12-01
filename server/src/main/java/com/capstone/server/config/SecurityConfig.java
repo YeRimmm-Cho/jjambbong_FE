d39c8e710b89d5dfaeab1cda0f643279bbc52f2d
gaaaani <geseo0523@gmail.com>
2024-12-01 22:53:09 +0900

    feat : [BE] 로그인 경로 수정

--- server/src/main/java/com/capstone/server/config/SecurityConfig.java
+++ server/src/main/java/com/capstone/server/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 인증 정보 포함 허용
-        config.setAllowedOrigins(List.of("http://172.20.10.3:3000")); // 프론트엔드 URL
+        config.setAllowedOrigins(List.of("https://hyunjong00.github.io/tamtam")); // 프론트엔드 URL
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용 메서드
         config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
 
@@ -66,7 +66,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://172.20.10.3:3000")); //v 프론트 url
+        config.setAllowedOrigins(List.of("https://hyunjong00.github.io/tamtam")); //v 프론트 url
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
