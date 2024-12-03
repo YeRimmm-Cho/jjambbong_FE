d642f84aa247e11005a1f5b6b0cea21def77c8e8
KIM SU HYUN <144208568+SuHyunKKim@users.noreply.github.com>
2024-12-03 17:32:52 +0900

    Update SecurityConfig.java

--- server/src/main/java/com/capstone/server/config/SecurityConfig.java
+++ server/src/main/java/com/capstone/server/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 인증 정보 포함 허용
-        config.setAllowedOrigins(List.of("http://localhost:3000", "https://HyunJong00.github.io", "https://hyunjong00.github.io/JJAMBBONG/")); // 프론트엔드 URL
+        config.setAllowedOrigins(List.of("https://tamtam2.shop", "http://localhost:3000", "https://HyunJong00.github.io", "https://hyunjong00.github.io/JJAMBBONG/")); // 프론트엔드 URL
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용 메서드
         config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
 
