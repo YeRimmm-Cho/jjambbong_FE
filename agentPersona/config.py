e10fbe60551b47332c796b4837ac1a58f9f8eaef
KIM SU HYUN <144208568+SuHyunKKim@users.noreply.github.com>
2024-12-18 20:12:34 +0900

    Update config.py

--- agentPersona/config.py
+++ agentPersona/config.py
@@ -6,10 +6,7 @@ load_dotenv()
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://admin:{DB_PASSWORD}@tamtam-db.cj2y4auwm7gh.ap-northeast-2.rds.amazonaws.com:3306/tamtamdb5'
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.abspath(os.path.join(BASE_DIR, "../data/testdb.sqlite"))}'
-
+SQLALCHEMY_DATABASE_URI ='sqlite:///C:/codespace/projects/capstone/data/testdb.sqlite'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
 SESSION_COOKIE_SECURE = False
